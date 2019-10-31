45093a3e3c53ced59a10c279f74584a6110ce136
William Acevedo <william.acevedo@gmail.com>
2019-10-31 09:45:40 +0100

    more testing on dropdown features

--- app/javascript/src/_navbar.js
+++ app/javascript/src/_navbar.js
@@ -1,54 +1,18 @@
-const navMenuButton = document.getElementByClass('.nav-menu-toggler');
-const navMenu = document.getElementByClass('.nav-right-side');
-navMenu.setAttribute('style', 'visibility: hidden;')
+const navToggleButton = document.getElementById('navToggler');
+const navMenu = document.getElementById('navMenu');
+let isOpen = false;
 
-function navMenuOpen() {
-  navMenuButton.classList.add('nav_rotate');
-  navMenu.classList.add('nav_show')
+function navToggle(isOpen) {
+
+  if (isOpen == false) {
+    isOpen = true;
+  } else {
+    isOpen = false;
+  }
+
+  console.log(isOpen);
 }
 
-function navMenuClose() {
-  navMenuButton.classList.remove('nav_rotate');
-  navMenu.classList.remove('nav_show')
-}
-
-navMenuButton.addEventListener('click', () => {
-    // (navMenu.classList.contains('nav_show')) ? navMenuOpen() : navMenuClose();
-    navMenuButton.classList.add('nav_show')
-    (navMenuButton.classList.contains('nav_show')) ? navMenuOpen() : navMenuClose();
+navToggleButton.addEventListener('click', () => {
+  navToggle();
 });
-
-
-
-
-
-
-
-
-
-// function navMenuOpen() {
-//   console.log('menu is open');
-// }
-//
-// function navMenuClose() {
-//   console.log('menu is closed');
-// }
-//
-// navMenuButton.addEventListener('onclick', () => {
-//   (navMenu.style.display == "none") ? navMenuOpen() : navMenuClose();
-// });
-//
-
-
-
-
-// function toggleNavMenu() {
-//
-//   if (navMenu.style.dsiplay == '') {
-//     navMenu.setAttribute('style', 'display:flex;');
-//   } else {
-//     navMenu.setAttribute('style', 'display:none;');
-//   }
-// }
-//
-// navMenuButton.addEventListener('click', toggleNavMenu);
