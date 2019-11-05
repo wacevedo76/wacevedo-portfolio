173574abcd5ce7fbc1cf3f871208b356ba836f7b
William Acevedo <william.acevedo@gmail.com>
2019-11-05 10:29:00 +0100

    completed Navtoggler feature, tested and verified

--- app/javascript/src/_navbar.js
+++ app/javascript/src/_navbar.js
@@ -1,17 +1,19 @@
 const navToggleButton = document.getElementById('navToggler');
 
-function toggleMenu(menu) {
-  // if (menu.style.display == '') {
-  //   menu.setAttribute('display', 'flex');
-  // } else {
-  //   menu.setAttribute('display', '');
-  // }
-  console.log(menu);
+function toggleMenu(e, menu) {
+  if (menu.style.display == '') {
+    e.target.classList.add('nav_rotate_open');
+    e.target.classList.remove('nav_rotate_close');
+    menu.style.display = 'flex';
+  } else {
+    e.target.classList.add('nav_rotate_close');
+    e.target.classList.remove('nav_rotate_open');
+    menu.style.display = '';
+  }
 };
-
 
 const navMenu = document.getElementById('navMenu');
 
-navToggleButton.addEventListener('click', () => {
-  toggleMenu(navMenu);
+navToggleButton.addEventListener('click', (e) => {
+  toggleMenu(e, navMenu);
 });
