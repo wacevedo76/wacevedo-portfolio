2c5014a38c982a3df9cb2b1e68a6c8569d8a6b99
William Acevedo <william.acevedo@gmail.com>
2022-08-12 17:07:15 +0200

    Reverts to "oldPortfolio". updates rails to 7.0.3

--- Gemfile
+++ Gemfile
@@ -1,76 +1,60 @@
-source "https://rubygems.org"
+source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
+ruby '3.1.2'
 
-ruby "3.1.2"
-
-# Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
-gem "rails", "~> 7.0.3"
-
-# The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
-gem "sprockets-rails"
-
-# Use sqlite3 as the database for Active Record
-# gem "sqlite3", "~> 1.4"
-gem 'pg'
-
-# Use the Puma web server [https://github.com/puma/puma]
-gem "puma", "~> 5.0"
-
-# Use JavaScript with ESM import maps [https://github.com/rails/importmap-rails]
-gem "importmap-rails"
-
-# Hotwire's SPA-like page accelerator [https://turbo.hotwired.dev]
-gem "turbo-rails"
-
-# Hotwire's modest JavaScript framework [https://stimulus.hotwired.dev]
-gem "stimulus-rails"
-
-# Use Tailwind CSS [https://github.com/rails/tailwindcss-rails]
-gem "tailwindcss-rails"
-
-# Build JSON APIs with ease [https://github.com/rails/jbuilder]
-gem "jbuilder"
+gem 'rails', '~> 7.0.3'
+gem 'pg', '>= 0.18', '< 2.0'
+gem 'puma', '~> 5.6'
+gem 'sassc-rails'
+gem 'webpacker', '~> 4.0'
+gem 'turbolinks', '~> 5'
+# Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
+gem 'jbuilder', '~> 2.7'
+# Reduces boot times through caching; required in config/boot.rb
+gem 'bootsnap', '>= 1.4.2', require: false
+gem 'actionview', '>= 6.0.2.2'
+gem 'font-awesome-sass', '~> 5.12.0'
+gem 'simple_form'
+gem 'devise'
+gem 'psych', '< 4'
+gem 'haml'
+gem 'haml-rails'
 
 # Use Redis adapter to run Action Cable in production
-# gem "redis", "~> 4.0"
-
-# Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
-# gem "kredis"
-
-# Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
-# gem "bcrypt", "~> 3.1.7"
-
-# Windows does not include zoneinfo files, so bundle the tzinfo-data gem
-gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
-
-# Reduces boot times through caching; required in config/boot.rb
-gem "bootsnap", require: false
-
-# Use Sass to process CSS
-# gem "sassc-rails"
-
-# Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
-# gem "image_processing", "~> 1.2"
+# gem 'redis', '~> 4.0'
+# Use Active Model has_secure_password
+# gem 'bcrypt', '~> 3.1.7'
+# Use Active Storage variant
+# gem 'image_processing', '~> 1.2'
 
 group :development, :test do
-  # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
-  gem "debug", platforms: %i[ mri mingw x64_mingw ]
+  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
 end
 
 group :development do
-  # Use console on exceptions pages [https://github.com/rails/web-console]
-  gem "web-console"
+  # Access an interactive console on exception pages or by calling 'console' anywhere in the code.
+  gem 'web-console', '>= 3.3.0'
+  gem 'listen', '>= 3.0.5', '< 3.2'
+  # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
+  gem 'spring'
+  gem 'spring-watcher-listen', '~> 2.0.0'
 
-  # Add speed badges [https://github.com/MiniProfiler/rack-mini-profiler]
-  # gem "rack-mini-profiler"
-
-  # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
-  # gem "spring"
+  # capistrano
+   gem 'capistrano', 		require: false
+   gem 'capistrano-rvm', 	require: false
+   gem 'capistrano-rails', 	require: false
+   gem 'capistrano-bundler', 	require: false
+   gem 'capistrano3-puma', 	require: false
 end
 
 group :test do
-  # Use system testing [https://guides.rubyonrails.org/testing.html#system-testing]
-  gem "capybara"
-  gem "selenium-webdriver"
-  gem "webdrivers"
+  # Adds support for Capybara system testing and selenium driver
+  gem 'capybara', '>= 2.15'
+  gem 'selenium-webdriver'
+  # Easy installation and use of web drivers to run system tests with browsers
+  gem 'webdrivers'
 end
+
+# Windows does not include zoneinfo files, so bundle the tzinfo-data gem
+gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
---
+++ public/packs/js/page-5c8d0920f10f30ffe3f1.js
@@ -0,0 +1,192 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/packs/";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./app/javascript/packs/page.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./app/javascript/packs/page.js":
+/*!**************************************!*\
+  !*** ./app/javascript/packs/page.js ***!
+  \**************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(/*! ../src/index */ "./app/javascript/src/index.js");
+
+/***/ }),
+
+/***/ "./app/javascript/src/_day-night.js":
+/*!******************************************!*\
+  !*** ./app/javascript/src/_day-night.js ***!
+  \******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+var dayModeEle = document.getElementById('dayMode');
+var nightModeEle = document.getElementById('nightMode');
+
+function dayNightModeSwitcher(modeChoice) {
+  var pageStyles = window.getComputedStyle(document.body);
+  var style = document.documentElement.style; // style.setProperty('--seen-primary', 'black');
+
+  if (modeChoice == 'day') {
+    style.setProperty('--seen-primary', 'black');
+    style.setProperty('--seen-secondary', 'blue');
+    style.setProperty('--h2-color', 'black');
+    style.setProperty('--bold-color', 'black');
+    style.setProperty('--seen-hover', 'blue');
+    style.setProperty('--space-primary', 'white');
+    style.setProperty('--space-secondary', 'white');
+  }
+
+  if (modeChoice == 'night') {
+    style.setProperty('--seen-primary', '#009688');
+    style.setProperty('--seen-secondary', 'blue');
+    style.setProperty('--h2-color', '#d77236');
+    style.setProperty('--bold-color', 'white');
+    style.setProperty('--seen-hover', 'blue');
+    style.setProperty('--space-primary', '#1d1f21');
+    style.setProperty('--space-secondary', '#1d1f21');
+  }
+}
+
+dayModeEle.addEventListener('click', function () {
+  dayNightModeSwitcher('day');
+});
+nightModeEle.addEventListener('click', function () {
+  dayNightModeSwitcher('night');
+});
+
+/***/ }),
+
+/***/ "./app/javascript/src/_navbar.js":
+/*!***************************************!*\
+  !*** ./app/javascript/src/_navbar.js ***!
+  \***************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+var navToggleButton = document.getElementById('navToggler');
+
+function toggleMenu(e, menu) {
+  if (menu.style.display == '') {
+    e.target.classList.add('nav_rotate_open');
+    e.target.classList.remove('nav_rotate_close');
+    menu.style.display = 'flex';
+  } else {
+    e.target.classList.add('nav_rotate_close');
+    e.target.classList.remove('nav_rotate_open');
+    menu.style.display = '';
+  }
+}
+
+;
+var navMenu = document.getElementById('navMenu');
+navToggleButton.addEventListener('click', function (e) {
+  toggleMenu(e, navMenu);
+});
+
+/***/ }),
+
+/***/ "./app/javascript/src/index.js":
+/*!*************************************!*\
+  !*** ./app/javascript/src/index.js ***!
+  \*************************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _navbar__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./_navbar */ "./app/javascript/src/_navbar.js");
+/* harmony import */ var _navbar__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_navbar__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _day_night__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./_day-night */ "./app/javascript/src/_day-night.js");
+/* harmony import */ var _day_night__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_day_night__WEBPACK_IMPORTED_MODULE_1__);
+
+
+
+/***/ })
+
+/******/ });
+//# sourceMappingURL=page-5c8d0920f10f30ffe3f1.js.map
